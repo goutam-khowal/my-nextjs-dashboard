9e8674f34886c1601a5b22ce54e6fe1f632ef368
GoutamK1902 <goutamkhowal321@gmail.com>
2025-06-17 19:04:49 +0530

    Initial Commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const InvoicesPage = () => {
+  return <div>Invoices</div>;
+};
+
+export default InvoicesPage;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <main className="">This is your DASHBOARD</main>;
+};
+
+export default page;
